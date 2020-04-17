a04046985adba4768c9a8a4d0972e51b4844b887
Adarsh Nanwani <adarsh.nanwani@gmail.com>
2020-04-17 22:49:54 +0530

    Add tests for other elements.

--- src/App.js
+++ src/App.js
@@ -4,8 +4,9 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div>
-        <h1>App</h1>
+      <div data-test='component-app'>
+        <h1 data-test='counter-display'>The counter is currently</h1>
+        <button data-test='increment-button'>Increment</button>
       </div>
     );
   }
