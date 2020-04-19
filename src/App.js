fc9c8c09bd6d132b1c471be5c12fea5639d5dae9
Adarsh Nanwani <adarsh.nanwani@gmail.com>
2020-04-19 20:29:58 +0530

    Write code to pass the tests.

--- src/App.js
+++ src/App.js
@@ -4,6 +4,23 @@ import './App.css';
 class App extends Component {
   state = {
     counter: 0,
+    error: false,
+  };
+
+  clickHandler = (val) => {
+    const currentCount = this.state.counter;
+    const newCount = currentCount + val;
+    if (newCount < 0) {
+      this.setState({
+        counter: 0,
+        error: true,
+      });
+    } else {
+      this.setState({
+        counter: newCount,
+        error: false,
+      });
+    }
   };
   render() {
     return (
@@ -13,20 +30,19 @@ class App extends Component {
         </h1>
         <button
           data-test='increment-button'
-          onClick={() =>
-            this.setState((prevState) => ({ counter: prevState.counter + 1 }))
-          }
+          onClick={() => this.clickHandler(1)}
         >
           Increment
         </button>
         <button
           data-test='decrement-button'
-          onClick={() =>
-            this.setState((prevState) => ({ counter: prevState.counter - 1 }))
-          }
+          onClick={() => this.clickHandler(-1)}
         >
           Decrement
         </button>
+        {this.state.error && (
+          <h4 data-test='error-message'>Counter can't go below zero</h4>
+        )}
       </div>
     );
   }
